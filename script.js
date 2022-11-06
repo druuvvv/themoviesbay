1c9be97880c671c8ae0ee36a811c474f2c8df2a8
Dhruv Mittal <dmittal1902@gmail.com>
2022-11-06 18:25:38 +0530

    new

--- script.js
+++ script.js
@@ -3,7 +3,8 @@
 let URL = "https://api.themoviedb.org/3/trending/movie/day?api_key=076113f04da878b9154d2580ddc6c02e";
 let poster_URL = "https://image.tmdb.org/t/p/w780";
 let upcoming_url = "https://api.themoviedb.org/3/movie/upcoming?api_key=076113f04da878b9154d2580ddc6c02e&language=en-US&page=1";
-
+const searchMovies = [];
+const searchIds = [];
 const poster_images = [];
 const titles = [];
 const movieIds = [];
@@ -51,10 +52,10 @@ fetch(upcoming_url).then(resp => {
             if (results[i]['release_date'] >= date) {
                 upcoming.push(results[i]['title']);
                 upcom_date.push(results[i]['release_date']);
-                newmovieId.push(results[i]['id']);
                 newposter.push(poster_URL + results[i]['poster_path']);
                 overviews.push(results[i]['overview']);
                 rating.push(results[i]['vote_average']);
+                movieId.push(results[i]['id']);
             }
         }
     }
@@ -71,7 +72,7 @@ fetch(upcoming_url).then(resp => {
             if (results[i]['release_date'] > date) {
                 upcoming.push(results[i]['title']);
                 upcom_date.push(results[i]['release_date']);
-                newmovieId.push(results[i]['id']);
+                movieId.push(results[i]['id']);
                 newposter.push(poster_URL + results[i]['poster_path']);
                 overviews.push(results[i]['overview']);
                 rating.push(results[i]['vote_average']);
@@ -92,7 +93,7 @@ fetch(upcoming_url).then(resp => {
             if (results[i]['release_date'] >= date) {
                 upcoming.push(results[i]['title']);
                 upcom_date.push(results[i]['release_date']);
-                newmovieId.push(results[i]['id']);
+                movieId.push(results[i]['id']);
                 overviews.push(results[i]['overview']);
                 newposter.push(poster_URL + results[i]['poster_path']);
                 rating.push(results[i]['vote_average']);
@@ -109,14 +110,6 @@ fetch(upcoming_url).then(resp => {
         console.log(upcom_date);
     }
 )
-
-
-function search(form){
-    let url = "https://api.themoviedb.org/3/search/movie?api_key=076113f04da878b9154d2580ddc6c02e&language=en-US&page=1&include_adult=false&query="
-    let searched = form.searchbox.value;
-    url = url + searched;
-}
-
 
 
 
@@ -157,6 +150,28 @@ function moreinfo(clickId){
         let cast4 = document.getElementById('cast4');
         cast4.innerText = character[1];
     })
+
+    let videourl = "https://api.themoviedb.org/3/movie/" + movieId[position] +"/videos?api_key=076113f04da878b9154d2580ddc6c02e&language=en-US";
+
+    fetch(videourl).then(resp => {
+        return resp.json();
+    }).then(json => {
+        let links = json['results'];
+
+        for(let i=0 ; i<links.length ; i++){
+            let utube = document.getElementById('youtube');
+            let uu = "https://www.youtube.com/embed/"+links[i]['key'];
+            if(links[i]['type'] == 'Trailer'){
+                utube.setAttribute('src' , uu);
+                utube.classList.remove('d-none');
+                break;
+
+            }
+            else{
+                utube.className = 'd-none';
+            }
+        }
+    })
 }
 
 function moreinfo2(clickId){
@@ -180,7 +195,7 @@ function moreinfo2(clickId){
     modalRating.innerText = rating[position+10].toFixed(1);
     
 
-    let url = "https://api.themoviedb.org/3/movie/" + newmovieId[position] + "/credits?api_key=076113f04da878b9154d2580ddc6c02e&language=en-US";
+    let url = "https://api.themoviedb.org/3/movie/" + movieId[position+10] + "/credits?api_key=076113f04da878b9154d2580ddc6c02e&language=en-US";
     console.log(url);
     const actors = [];
     const character = [];
@@ -201,6 +216,30 @@ function moreinfo2(clickId){
         let cast4 = document.getElementById('cast4');
         cast4.innerText = character[1];
     })
+
+    let videourl = "https://api.themoviedb.org/3/movie/" + movieId[position+10] +"/videos?api_key=076113f04da878b9154d2580ddc6c02e&language=en-US";
+
+    fetch(videourl).then(resp => {
+        return resp.json();
+    }).then(json => {
+        let links = json['results'];
+
+        for(let i=0 ; i<links.length ; i++){
+            let utube = document.getElementById('youtube');
+            let uu = "https://www.youtube.com/embed/"+links[i]['key'];
+            if(links[i]['type'] == 'Trailer'){
+                utube.setAttribute('src' , uu);
+                utube.className = '';
+                break;
+
+            }
+            else{
+                utube.className = 'd-none';
+            }
+        }
+    })
+
+
 }
 
 if(screen.availWidth < 736){
@@ -222,4 +261,43 @@ if(screen.availWidth < 736){
     columns[2].classList.add('h-50');
 }
 
-console.log(overviews);
+function search(form){
+    let searchURL = "https://api.themoviedb.org/3/search/movie?api_key=076113f04da878b9154d2580ddc6c02e&language=en-US&page=1&include_adult=false&query="
+    let searched = form.searchbox.value;
+    let btnsearch = document.getElementById('btnsearch');
+    searched = searched.replaceAll(" " , "+");
+    searchURL = searchURL + searched;
+    btnsearch.click();
+    fetch(searchURL).then(response => {
+        return response.json();
+    }).then(json => {
+        let searchjson = json['results'];
+        let searchList = document.querySelectorAll('.search-list');
+        for(let i=0;i<5;i++){
+
+            searchMovies[i] = searchjson[i]['original_title'];
+            searchIds[i] = searchjson[i]['id'];
+        }
+
+
+        for(let i=0;i<5;i++){
+            searchList[i].innerText = searchMovies[i];
+        }
+        
+    })
+   
+}
+function searchinfo(clicksearch){
+    let position = clicksearch.charCodeAt(7) - 48;
+    let findURL  = "https://api.themoviedb.org/3/movie/" + searchIds[position] + "?api_key=076113f04da878b9154d2580ddc6c02e&language=en-US";
+
+    fetch(findURL).then(resp => {
+        return resp.json();
+    }).then(json => {
+         const ext = json['homepage'];
+        console.log(ext + " " + searchIds[position]);
+        
+    })  
+}
+
+
